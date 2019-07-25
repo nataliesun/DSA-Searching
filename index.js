c40393aeae26faae7d791bee51b25ffef1087714
Mengqi Wang <wmq516@gmail.com>
2019-07-25 17:38:17 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+class BinarySearchTree {
+    constructor(key = null, value = null, parent = null) {
+        this.key = key;
+        this.value = value;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+
+    dfs(values = []) {
+        if (this.left) {
+            values = this.left.dfs(values);
+        }
+        values.push(this.value);
+
+        if (this.right) {
+            values = this.right.dfs(values);
+        }
+        return values;
+    }
+
+    bfs(values = []) {
+        const queue = new Queue(); // Assuming a Queue is implemented (refer to previous lesson on Queue)
+
+        while (queue.length) {
+            const node = queue.dequeue(); //remove from the queue
+            values.push(node.value); // add that value from the queue to an array
+
+            if (node.left) {
+                queue.enqueue(node.left); //add left child to the queue
+            }
+
+            if (node.right) {
+                queue.enqueue(node.right); // add right child to the queue
+            }
+        }
+
+        return values;
+    }
+}
+
+
+
+// 1. How many searches ?
+//     Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and are using the recursive binary search algorithm.Identify the sequence of numbers that each recursive call will search to find 8.
+
+// 3 searches: 11 - 6 - 8
+
+// Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and are using the recursive binary search algorithm.Identify the sequence of numbers that each recursive call will search to find 16 ?
